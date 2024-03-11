349faa94846a6ab10d8d534e4094d33e46f62699
mishalalshahari <mishalalshahari@gmail.com>
2024-03-11 15:50:18 +0530

    middleware and errorhandler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password:hashedPassword });
@@ -11,6 +11,7 @@ export const register = async (req, res) => {
         res.status(201).json({ message: "User registered successfully!" });
     }
     catch (err) {
-        res.status(500).json({ message: err.message });
+        // next(errorHandler(300, "customised error message"));
+        next(err);
     }
 };
--- api/index.js
+++ api/index.js
@@ -30,3 +30,13 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+});
