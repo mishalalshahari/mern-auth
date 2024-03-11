5964227fdde511be0002e9c2b6fe501ea3d0b985
mishalalshahari <mishalalshahari@gmail.com>
2024-03-11 14:42:42 +0530

    added user controller and route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+
 
 dotenv.config();
 
@@ -17,3 +19,11 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!'); 
 });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    })
+});
+
+app.use('/api/users', userRoutes);
